8c25a232c0fb099215ada1aa88761dbd980d30ce
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-07-17 19:16:28 +0200

    axios params

--- src/js/API(pixabay).js
+++ src/js/API(pixabay).js
@@ -1,11 +1,24 @@
+// import AXIOS library to make creating async functions more automatical and quicker
 import axios from "axios";
 
-// adding all of necessary information to the API from https://pixabay.com/api/docs/
-const baseURL = 'https://pixabay.com/api/';
-const myPixabayKey = "44872402-0762bbf0a5ccd686fb6258473";
-const image_type = 'photo';
-const orientation = 'horizontal';
-const safesearch = true;
+const myApiKey = "44872402-0762bbf0a5ccd686fb6258473";
+axios.defaults.baseURL = "<https://pixabay.com/>";
+
+function getPictures(imageQuery, page=1) {
+    const queryResponse =  axios.get("/api", {
+        params: {
+        key: myApiKey,
+        q: imageQuery,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+        per_page: 15,
+        page: page,
+        }
+    });
+    return queryResponse.data
+ }
+
 
 // making a query to the API that returns answers for user's searching input
 export function getPictures(query) {
