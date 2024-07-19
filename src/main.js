0bf4a7089161ec6fb44cce710e4b08b5eefb937b
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-07-19 22:09:32 +0200

    pre-define scope of works

--- src/main.js
+++ src/main.js
@@ -1,24 +1,22 @@
-import { getPictures } from "./js/API(pixabay).js";
-import { createMarkup, catchError } from "./js/galleryMarkups.js";
+// PRE-settings in MAIN.js
+// 1. import pop - ups library from iziToasts;
+// 2. import gallery library from simplelightbox; 
+// 3. import API settings;
+// 4. import gallery making functions;
 
-const searchForm = document.querySelector(".search-form");
-const cardContainer = document.querySelector(".card-container");
-const loader = document.querySelector(".loader");
-searchForm.addEventListener("submit", handlerSearch);
+// event listeners settings
+// 5. const form;
+// 6. const input;
+// 7. const gallery
+// 8. const loader
 
-function handlerSearch(event) {
-    event.preventDefault();
+// local variables "LET"
+// 9. current page;
+// 10. min page;
+// 11. max page;
+// 12. responses view by page(ex. 20, 30 etc);
+// 13. image query(searching images)
+// 14. simple lightbox gallery settings
 
-    const form = event.currentTarget;
-    const userValue = form.elements.search.value.toLowerCase();
 
-    loader.style.display = "block";
 
-    getPictures(userValue)
-        .then((data) => createMarkup(data, cardContainer))
-        .catch(catchError)
-        .finally(() => {
-            form.reset();
-            loader.style.display = "none";
-        });
-}
