31ef88a747630d86087aadc479ec4f8e4f658bf9
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-07-20 17:28:54 +0200

    API settings

--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
 // re-fresh API and gallery mark-up
+import axios from "axios";
+const API_KEY = "44872402-0762bbf0a5ccd686fb6258473";
+const BASE_URL = "https://pixabay.com/api/"
+
+async function getImages(query, page) { 
+    const params = {
+        key: API_KEY,
+        q: query,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+        page: page,
+        per_page: 20,
+    };
+
+    try {
+        const response = await axios.get(BASE_URL, { params });
+        return response.data;
+        } catch (error) {
+        throw new Error("Fetch request failed");
+     }
+    }
+
+
 
 // PRE-settings in MAIN.js
 // 1. import pop - ups library from iziToasts;
