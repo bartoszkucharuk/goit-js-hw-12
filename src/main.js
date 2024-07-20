7b7d51bdb01115f4dd13d0ff681942e3aec3b1d6
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-07-20 17:11:59 +0200

    hide to-do list

--- src/main.js
+++ src/main.js
@@ -1,34 +1,34 @@
-PRE-settings in MAIN.js
-1. import pop - ups library from iziToasts;
-2. import gallery library from simplelightbox;
-3. import API settings;
-4. import gallery making functions;
+// PRE-settings in MAIN.js
+// 1. import pop - ups library from iziToasts;
+// 2. import gallery library from simplelightbox;
+// 3. import API settings;
+// 4. import gallery making functions;
 
-event listeners settings
-5. const form;
-6. const input;
-7. const gallery
-8. const loader
+// event listeners settings
+// 5. const form;
+// 6. const input;
+// 7. const gallery
+// 8. const loader
 
-local variables "LET"
-9. current page;
-10. min page;
-11. max page;
-12. responses view by page(ex. 20, 30 etc);
-13. image query(searching images)
-14. simple lightbox gallery settings
+// local variables "LET"
+// 9. current page;
+// 10. min page;
+// 11. max page;
+// 12. responses view by page(ex. 20, 30 etc);
+// 13. image query(searching images)
+// 14. simple lightbox gallery settings
 
-starting functionally of search page
+// starting functionally of search page
 
-set form submit button to listener and star query
-set default page
+// set form submit button to listener and star query
+// set default page
 
-LOADER- make it visible
-hide load-more button
-inner html as a gallery markup start
+// LOADER- make it visible
+// hide load-more button
+// inner html as a gallery markup start
 
-making IF formule for iziToasts, for check if the query isn't empty, hide loader and get whole code as response [return response]
+// making IF formule for iziToasts, for check if the query isn't empty, hide loader and get whole code as response [return response]
 
-TRY / CATCH
+// TRY / CATCH
 
 
