466d052f2e36f8af548716b6cc70a53b4c4ff39e
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-07-21 11:17:27 +0200

    event listeners and "load more button"

--- src/main.js
+++ src/main.js
@@ -2,17 +2,35 @@
     // DONE
 
 // PRE-settings in MAIN.js
+
+
+
+import iziToast from "iziToast";
+import "iziToast/dist/css/iziToast.min.css";
+import SimpleLightBox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { getImages } from "./js/API_pixabay.js";
+import { createMarkup } from "./js/galleryMarkups.js";
 // 1. import pop - ups library from iziToasts;
 // 2. import gallery library from simplelightbox;
 // 3. import API settings;
 // 4. import gallery making functions;
     // DONE
 
+const form = document.querySelector(".search-form");
+const input = document.querySelector(".search-input");
+const gallery = document.querySelector(".card-container");
+const loader = document.querySelector(".loader");
+const loadMoreButton = document.querySelector(".load-more-button")
+
+
 // event listeners settings
 // 5. const form;
 // 6. const input;
 // 7. const gallery
 // 8. const loader
+    // DONE
 
 // local variables "LET"
 // 9. current page;
@@ -34,14 +52,6 @@
 // making IF formule for iziToasts, for check if the query isn't empty, hide loader and get whole code as response [return response]
 
 // TRY / CATCH
-
-import iziToast from "iziToast";
-import "iziToast/dist/css/iziToast.min.css";
-import SimpleLightBox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-import { getImages } from "./js/API_pixabay.js";
-import { createMarkup } from "./js/galleryMarkups.js";
 
 
 
