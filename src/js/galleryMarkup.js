58327596c97a6475bde57d168fefb80b94196ae3
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-07-22 18:00:28 +0200

    gallery almost done

--- src/js/galleryMarkup.js
+++ src/js/galleryMarkup.js
@@ -8,31 +8,31 @@ export function createMarkup(data) {
   return data.map(image => `
     <li class="gallery-item">
 
-      <a href="${image.largeImageURL}">
+      <a href="${image.largeImageURL}" class="gallery-link">
           <img class="card-image"
             src="${image.webformatURL}"
             alt="${image.tags}"
             width="360">
       </a>
 
-    <ul class="card-description-list">
-        <li class="card-description-item">
-            <h3 class="card-description-title">Likes</h3>
-            <p class="card-description-value">${image.likes}</p>
-        </li>
-        <li class="card-description-item">
-            <h3 class="card-description-title">Views</h3>
-            <p class="card-description-value">${image.views}</p>
-        </li>
-        <li class="card-description-item">
-            <h3 class="card-description-title">Comments</h3>
-            <p class="card-description-value">${image.comments}</p>
-        </li>
-        <li class="card-description-item">
-            <h3 class="card-description-title">Downloads</h3>
-            <p class="card-description-value">${image.downloads}</p>
-        </li>
-    </ul>
+      <ul class="card-description-list">
+          <li class="card-description-item">
+              <h3 class="card-description-title">Likes</h3>
+              <p class="card-description-value">${image.likes}</p>
+          </li>
+          <li class="card-description-item">
+              <h3 class="card-description-title">Views</h3>
+              <p class="card-description-value">${image.views}</p>
+          </li>
+          <li class="card-description-item">
+              <h3 class="card-description-title">Comments</h3>
+              <p class="card-description-value">${image.comments}</p>
+          </li>
+          <li class="card-description-item">
+              <h3 class="card-description-title">Downloads</h3>
+              <p class="card-description-value">${image.downloads}</p>
+          </li>
+      </ul>
   </li>
 `).join('');
 }
